f41df6974e76636fbbdc30f927c253eb105ae1b5
nermeen_atif <1701571@eng.asu.edu.eg>
2021-05-27 02:21:41 +0200

    main function,hole function,segment function and mem history function

---
+++ Hole.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mem_allocation
+{
+    class Hole
+    {
+        private int Hole_ID;
+        private int Starting_Address;
+        private int Size;
+        public Hole()
+        {
+        }
+        public void set_Hole_ID(int Hole_ID)
+        {
+            this.Hole_ID = Hole_ID;
+        }
+        public int get_Hole_ID()
+        {
+            return this.Hole_ID;
+        }
+        public void set_Starting_Address(int Starting_Address)
+        {
+            this.Starting_Address = Starting_Address;
+        }
+        public int get_Starting_Address()
+        {
+            return this.Starting_Address;
+        }
+        public void set_Size(int Size)
+        {
+            this.Size = Size;
+        }
+        public int get_Size()
+        {
+            return this.Size;
+        }
+
+    }
+}
