81a74ea271d74e5902b36e56b21045002be24f12
Mahinour Amr <mahinouramr1998@gmail.com>
2021-05-27 23:25:01 +0200

    history list and old processes

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace mem_allocation
             int num_process;//number of process
             List<Hole> hole_list = new List<Hole>();
             List<Segment> segment_list = new List<Segment>();
+            List<Mem_History> history_list = new List<Mem_History>();
             string type_method;
             Console.WriteLine("enter mem_size ");
             mem_size = Convert.ToInt32(Console.ReadLine());
@@ -29,9 +30,71 @@ namespace mem_allocation
                 Console.WriteLine("enter size for hole " + i);
                 hole.set_Size(Convert.ToInt32(Console.ReadLine()));
                 hole_list.Add(hole);
+                // Adding holes in history_list
+                Mem_History history_element = new Mem_History();
+                history_element.set_Name("Hole"+i);
+                history_element.set_Start(hole.get_Starting_Address());
+                history_element.set_End((hole.get_Starting_Address())+ (hole.get_Size())-1);
+                history_list.Add(history_element);
 
 
             }
+            // Handling Old Processes
+            // sorting hole list by starting address
+            hole_list = hole_list.OrderBy(Hole => Hole.get_Starting_Address()).ToList();
+            int old_number = -1; // old process id
+            int start=0;
+            for (int i = 0; i < hole_list.Count; i++)
+            {
+                if (i == 0 && hole_list.Count>1)
+                {
+                    if (hole_list[i].get_Starting_Address() == 0)
+                        continue;
+                    else { start = 0; }
+                }
+                else if (i == (hole_list.Count - 1))
+                {
+                    if (i == 0 && (hole_list[i].get_Starting_Address() != 0))
+                        start = 0;
+                    else if ((hole_list[i].get_Starting_Address()) + (hole_list[i].get_Size()) < mem_size)
+                    {
+                        if (i > 0) old_number -= 1;
+                        // putting last old process in history
+                        Mem_History history_element_last = new Mem_History();
+                        history_element_last.set_Name("Old Process"+(-1*(old_number)));
+                        history_element_last.set_Start((hole_list[i].get_Starting_Address())+ hole_list[i].get_Size());
+                        history_element_last.set_End(mem_size-1);
+                        history_list.Add(history_element_last);
+                        // putting last old process in segment
+                        Segment segment_last = new Segment();
+                        segment_last.set_Name("Old Process" + (-1 * (old_number)));
+                        segment_last.set_Process_ID(old_number);
+                        segment_last.set_Size((history_element_last.get_End())-(history_element_last.get_Start()) +1);
+                        segment_list.Add(segment_last);
+                        if (i == 0)
+                            continue;
+                        else { start = (hole_list[i - 1].get_Starting_Address()) + (hole_list[i - 1].get_Size()); old_number += 1; }
+
+                    }
+                    
+                    else { start = (hole_list[i - 1].get_Starting_Address()) + (hole_list[i - 1].get_Size()); }
+                }
+                else { start = (hole_list[i - 1].get_Starting_Address())+(hole_list[i - 1].get_Size()); }
+                // putting old process in history_list
+                Mem_History history_element = new Mem_History();
+                history_element.set_Name("Old Process" + (-1 * old_number));
+                history_element.set_Start(start);
+                history_element.set_End(hole_list[i].get_Starting_Address()-1);
+                history_list.Add(history_element);
+                // putting old process in segment_list
+                Segment segment = new Segment();
+                segment.set_Name("Old Process" + (-1 * old_number));
+                segment.set_Process_ID(old_number);
+                segment.set_Size((hole_list[i].get_Starting_Address()) - start);
+                segment_list.Add(segment);
+                old_number--;
+            }
+              
             Console.WriteLine("enter number of process ");
             num_process = Convert.ToInt32(Console.ReadLine());
             //enter all processes
@@ -50,12 +113,16 @@ namespace mem_allocation
                     Console.WriteLine("enter size for Segment " + i);
                     segment.set_Size(Convert.ToInt32(Console.ReadLine()));
                     segment_list.Add(segment);
+                    
                 }
 
             }
             Console.WriteLine("enter  method of allocation (first fit or best fit). " );
             type_method = Console.ReadLine();
+           
 
         }
+
+        
     }
 }
