eb9720293c4b4f547bd8ce5e4945b1102781a9fc
nermeen_atif <1701571@eng.asu.edu.eg>
2021-05-30 00:39:46 +0200

    add function

--- Classes/Segment.cs
+++ Classes/Segment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace classes
 {
@@ -40,6 +41,141 @@ namespace classes
             return this.Process_ID;
         }
 
+        public void allocate( Hole h,ref List<Mem_History> history_list,ref List<Hole> hole_list)
+        {
 
+            //sort history_list 
+            history_list = history_list.OrderBy(s => s.get_Start()).ToList();
+
+            //sort holes_list
+            hole_list=hole_list.OrderBy(s=>s.get_Starting_Address()).ToList();
+            
+            int difference = h.get_Size()-this.get_Size();
+            if (difference < 0)
+            {
+               // Console.WriteLine("the segment is larger than the hole");
+                return;
+            }
+            //insert the segment in the hole choosen
+            for (int i = 0; i < history_list.Count; i++)
+            {
+                if(history_list[i].get_Start()==h.get_Starting_Address())
+                {
+                    history_list[i].set_Name ("P"+this.get_Process_ID() +":"+this.get_Name());
+                    history_list[i].set_Id(this.get_Process_ID());
+                    history_list[i].set_End(history_list[i].get_Start()+this.get_Size()-1);
+                    
+                    break;
+                }
+            }
+            // adjust the remaining part of the hole
+            for (int i=0;i<hole_list.Count;i++)
+            {
+                if (hole_list[i].get_Hole_ID()==h.get_Hole_ID())
+                {
+                    //if there is no remaining space in the hole then remove the entry of this hole from holes_list
+                    if (difference == 0)
+                    {
+                        hole_list.RemoveAt(i);
+                        //renamin the holes after removing one 
+                        hole_list = hole_list.OrderBy(Hole => Hole.get_Starting_Address()).ToList();
+                        for (int j = 0; j < hole_list.Count; j++)
+                        {
+                            hole_list[j].set_Hole_ID(j);
+                            int add = hole_list[j].get_Starting_Address();
+                            history_list[history_list.FindIndex(a => a.get_Start() == add)].set_Name("Hole" + j);
+                            
+                        }
+
+                    }
+                    // add an entry in history_list for the remaining space in the hole
+                    else
+                    {
+                        //adjust the size of the hole 
+                        hole_list[i].set_Size(hole_list[i].get_Size() - this.get_Size());
+                        hole_list[i].set_Starting_Address(hole_list[i].get_Starting_Address() + this.get_Size());
+                        // add an entry in history_list for the remaining space in the hole
+                        Mem_History m =new Mem_History();
+                        m.set_Name("Hole" + h.get_Hole_ID());
+                        m.set_Start(hole_list[i].get_Starting_Address());
+                        m.set_End(hole_list[i].get_Starting_Address() + hole_list[i].get_Size()-1);
+                        history_list.Add(m);
+                        break;
+                    }
+                    
+                }
+            }
+            history_list = history_list.OrderBy(s => s.get_Start()).ToList();
+            hole_list = hole_list.OrderBy(s => s.get_Starting_Address()).ToList();
+            
+        }
+        public void Deallocate(ref List<Mem_History> history_list, ref List<Hole> hole_list)
+        {
+            int find1 = 0; int find2 = 0; int find3 = 0;
+            // history must be sorted before anything
+            history_list = history_list.OrderBy(Hole => Hole.get_Start()).ToList();
+            // to get name and search in history list
+            string name;
+            if (this.Name.StartsWith("O")) // old process
+                name = this.Name;
+            else // segment of a new process
+                name = "P" + this.Process_ID + ":" + this.Name;
+            // search in history by this name and get the index
+            int history_index = history_list.FindIndex(a => a.get_Name() == name);
+            if (history_index == -1)
+            { }
+            else
+            {
+                // To check for adjacent upper hole 
+                if (history_index > 0)
+                {
+                    if ((history_list[history_index - 1].get_Name().StartsWith("H")))
+                    {
+                        // combine 2 holes
+                        history_list[history_index].set_Start(history_list[history_index - 1].get_Start());
+                        // remove upper hole from hole list
+                        find1 = history_list[history_index - 1].get_Start();
+                        hole_list.RemoveAt(hole_list.FindIndex(a => a.get_Starting_Address() == find1));
+                        // remove upper hole from history list
+                        history_list.RemoveAt(history_index - 1);
+                        history_index--;
+
+                    }
+                }
+
+                // To check for adjacent lower hole
+                if (history_index < (history_list.Count - 1))
+                {
+                    if ((history_list[history_index + 1].get_Name().StartsWith("H")))
+                    {
+                        // combine 2 holes
+                        history_list[history_index].set_End(history_list[history_index + 1].get_End());
+                        // remove lower hole from hole list
+                        find2 = history_list[history_index + 1].get_Start();
+                        hole_list.RemoveAt(hole_list.FindIndex(a => a.get_Starting_Address() == find2));
+                        // remove lower hole from history list
+                        history_list.RemoveAt(history_index + 1);
+                    }
+                }
+
+                // To insert the new hole in hole list
+                Hole new_hole = new Hole();
+                new_hole.set_Starting_Address(history_list[history_index].get_Start());
+                new_hole.set_Size((history_list[history_index].get_End()) - (history_list[history_index].get_Start()) + 1);
+                hole_list.Add(new_hole);
+                // To give this new hole id and update rest of ids and change name in history 
+                hole_list = hole_list.OrderBy(Hole => Hole.get_Starting_Address()).ToList();
+                for (int i = 0; i < hole_list.Count; i++)
+                {
+                    hole_list[i].set_Hole_ID(i);
+                    find3 = hole_list[i].get_Starting_Address();
+                    history_list[history_list.FindIndex(a => a.get_Start() == find3)].set_Name("Hole" + i);
+                }
+                // to sort history again
+                history_list = history_list.OrderBy(Hole => Hole.get_Start()).ToList();
+            }
+
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ using System.Text;
 using classes;
 namespace mem_allocation
 {
+   
+
     class Program
     {
+        
         static void Main(string[] args)
         {
             int mem_size;
@@ -15,26 +18,26 @@ namespace mem_allocation
             List<Hole> hole_list = new List<Hole>();
             List<Segment> segment_list = new List<Segment>();
             List<Mem_History> history_list = new List<Mem_History>();
-            string type_method;
-            Console.WriteLine("enter mem_size ");
+            //string type_method;
+            //Console.WriteLine("enter mem_size ");
             mem_size = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter number of holes ");
+            //Console.WriteLine("enter number of holes ");
             num_holes = Convert.ToInt32(Console.ReadLine());
             //enter all holes
-            for(int i = 0; i < num_holes; i++)
+            for (int i = 0; i < num_holes; i++)
             {
                 Hole hole = new Hole();// var to take hole from user and adding to hole_list
-                Console.WriteLine("enter  starting address for hole "+ i);
+                //Console.WriteLine("enter  starting address for hole " + i);
                 hole.set_Hole_ID(i);
                 hole.set_Starting_Address(Convert.ToInt32(Console.ReadLine()));
-                Console.WriteLine("enter size for hole " + i);
+                //Console.WriteLine("enter size for hole " + i);
                 hole.set_Size(Convert.ToInt32(Console.ReadLine()));
                 hole_list.Add(hole);
                 // Adding holes in history_list
                 Mem_History history_element = new Mem_History();
-                history_element.set_Name("Hole"+i);
+                history_element.set_Name("Hole" + i);
                 history_element.set_Start(hole.get_Starting_Address());
-                history_element.set_End((hole.get_Starting_Address())+ (hole.get_Size())-1);
+                history_element.set_End((hole.get_Starting_Address()) + (hole.get_Size()) - 1);
                 history_list.Add(history_element);
 
 
@@ -43,10 +46,10 @@ namespace mem_allocation
             // sorting hole list by starting address
             hole_list = hole_list.OrderBy(Hole => Hole.get_Starting_Address()).ToList();
             int old_number = -1; // old process id
-            int start=0;
+            int start = 0;
             for (int i = 0; i < hole_list.Count; i++)
             {
-                if (i == 0 && hole_list.Count>1)
+                if (i == 0 && hole_list.Count > 1)
                 {
                     if (hole_list[i].get_Starting_Address() == 0)
                         continue;
@@ -55,47 +58,50 @@ namespace mem_allocation
                 else if (i == (hole_list.Count - 1))
                 {
                     if (i == 0 && (hole_list[i].get_Starting_Address() != 0) && ((hole_list[i].get_Starting_Address()) + (hole_list[i].get_Size()) == mem_size))
-                    {  
-                       start = 0;
-                        
+                    {
+                        start = 0;
+
                     }
-                         
+
                     else if ((hole_list[i].get_Starting_Address()) + (hole_list[i].get_Size()) < mem_size)
                     {
-                        if ((i > 0) | ((i==0)&&(hole_list[i].get_Starting_Address() != 0))) old_number -= 1;
+                        if ((i > 0) | ((i == 0) && (hole_list[i].get_Starting_Address() != 0))) old_number -= 1;
                         // putting last old process in history
                         Mem_History history_element_last = new Mem_History();
-                        history_element_last.set_Name("Old Process"+(-1*(old_number)));
-                        history_element_last.set_Start((hole_list[i].get_Starting_Address())+ hole_list[i].get_Size());
-                        history_element_last.set_End(mem_size-1);
+                        history_element_last.set_Name("Old Process" + (-1 * (old_number)));
+                        history_element_last.set_Start((hole_list[i].get_Starting_Address()) + hole_list[i].get_Size());
+                        history_element_last.set_End(mem_size - 1);
+                        history_element_last.set_Id(old_number);//here
                         history_list.Add(history_element_last);
                         // putting last old process in segment
                         Segment segment_last = new Segment();
                         segment_last.set_Name("Old Process" + (-1 * (old_number)));
                         segment_last.set_Process_ID(old_number);
-                        segment_last.set_Size((history_element_last.get_End())-(history_element_last.get_Start()) +1);
+                        segment_last.set_Size((history_element_last.get_End()) - (history_element_last.get_Start()) + 1);
                         segment_list.Add(segment_last);
                         if ((i == 0) && (hole_list[i].get_Starting_Address() == 0))
                             continue;
-                        else 
+                        else
                         {
                             if (i > 0)
                             { start = (hole_list[i - 1].get_Starting_Address()) + (hole_list[i - 1].get_Size()); old_number += 1; }
-                           
+
                             else if (i == 0)
-                                start = 0; old_number += 1;
+                            { start = 0; old_number += 1; }
+
                         }
 
                     }
-                    
+
                     else { start = (hole_list[i - 1].get_Starting_Address()) + (hole_list[i - 1].get_Size()); }
                 }
-                else { start = (hole_list[i - 1].get_Starting_Address())+(hole_list[i - 1].get_Size()); }
+                else { start = (hole_list[i - 1].get_Starting_Address()) + (hole_list[i - 1].get_Size()); }
                 // putting old process in history_list
                 Mem_History history_element = new Mem_History();
                 history_element.set_Name("Old Process" + (-1 * old_number));
                 history_element.set_Start(start);
-                history_element.set_End(hole_list[i].get_Starting_Address()-1);
+                history_element.set_End(hole_list[i].get_Starting_Address() - 1);
+                history_element.set_Id(old_number);
                 history_list.Add(history_element);
                 // putting old process in segment_list
                 Segment segment = new Segment();
@@ -106,119 +112,88 @@ namespace mem_allocation
                 old_number--;
             }
 
-            Console.WriteLine("enter number of process ");
+            //Console.WriteLine("enter number of process ");
             num_process = Convert.ToInt32(Console.ReadLine());
             //enter all processes
-            for(int j=0;j< num_process; j++)
+            for (int j = 0; j < num_process; j++)
             {
                 int num_segment;//number of segment
-                Console.WriteLine("enter number of segment for process "+j);
+                //Console.WriteLine("enter number of segment for process " + j);
                 num_segment = Convert.ToInt32(Console.ReadLine());
                 //enter all segments
                 for (int i = 0; i < num_segment; i++)
-                {             
+                {
                     Segment segment = new Segment();// var to take Segment from user and adding to Segment_list
-                    Console.WriteLine("enter  name of Segment " + i);
+                    //Console.WriteLine("enter  name of Segment " + i);
                     segment.set_Process_ID(j);
                     segment.set_Name(Console.ReadLine());
-                    Console.WriteLine("enter size for Segment " + i);
+                    //Console.WriteLine("enter size for Segment " + i);
                     segment.set_Size(Convert.ToInt32(Console.ReadLine()));
                     segment_list.Add(segment);
-                    
+
                 }
 
             }
-            Console.WriteLine("enter  method of allocation (first fit or best fit). ");
-            string s;
-            int flag ;
-            int process_id_d;//process id of delete current segment
-            int delete;
-            string name_delete_process;
-             
-            type_method = Console.ReadLine();
-            switch (type_method)
+            //Console.WriteLine("enter  method of allocation (first fit or best fit). ");
+            //type_method = Console.ReadLine();
+            int p_id=0;
+            Boolean b = First_Fit(ref segment_list,ref  history_list,ref  hole_list,ref p_id);
+
+
+        }
+       public static Boolean First_Fit(ref List<Segment> segment_list,ref List<Mem_History> history_list,ref List<Hole> hole_list,ref int p_id)
+        {
+            int flag;
+            //Mem_History index =new Mem_History();//result of find segment in meme history
+            int index1;
+            int index2;
+            //Segment s = new Segment();
+            for(int i=0;i< segment_list.Count(); i++)
             {
-              case  "first fit" :
-                    for(int i=0;i< segment_list.Count(); i++)
+                int add = segment_list[i].get_Process_ID();
+                string s = segment_list[i].get_Name();
+                flag = 0;
+                index1 = history_list.FindIndex(x=>x.get_Name()==
+                ("p"+Convert.ToString(add) +":"+ s));
+                index2 = history_list.FindIndex(x => x.get_Name() == s);
+
+                if (index1 == -1&& index2==-1)
+                {
+                    for (int j = 0; j < history_list.Count(); j++)
                     {
-                        flag = 0;
-                        for (int j=0;j< history_list.Count(); j++)
+
+                        if ((history_list[j].get_End() - history_list[j].get_Start() + 1)
+                            >= segment_list[i].get_Size() && history_list[j].get_Id() == null)
                         {
-                            s = history_list[j].get_Name();
-
-
-                           if ( s.Substring(0,4) == "Hole")
+                            Hole h = new Hole();
+                            for (int k = 0; k < hole_list.Count(); k++)
                             {
-                                if((history_list[j].get_End()- history_list[j].get_Start()+1)== segment_list[i].get_Size())
+                                if (hole_list[k].get_Starting_Address() == history_list[j].get_Start())
                                 {
-                                    // alocate function
-                                    flag = 1;
-                                    break;
-                                    
+                                    h = hole_list[k];
                                 }
 
                             }
 
+                            segment_list[i].allocate(h,ref  history_list,ref  hole_list);
+                            flag = 1;
+                            break;
                         }
-                        if (flag == 0)
-                        {
-                            Console.WriteLine("0 or 1 " );//if delete =1 want to delete
-                            delete = Convert.ToInt32(Console.ReadLine());
-                            if (delete == 1)
-                            {
-                                Console.WriteLine("which process want to delete ");
-                                name_delete_process = Console.ReadLine();
-                                if (name_delete_process[0] == 'P')
-                                {
-                                    for (int k = 0; k < segment_list.Count(); k++)
-                                    {
-                                        if (Convert.ToInt32(name_delete_process.Substring(1)) == segment_list[k].get_Process_ID())
-                                        {
-                                            //delete segment
-                                            //dealocate
-                                            //segment_list[i].dealocate(history_list);
-                                        }
-                                    }
 
-
-                                }
-                                else
-                                {
-                                    for(int k=0;k< segment_list.Count(); k++)
-                                    {
-                                        if(name_delete_process== segment_list[k].get_Name())
-                                        {
-                                            //delete segment
-                                            //dealocate
-                                        }
-                                    }
-                                }
-
-                            }
-                            else
-                            {
-                                //delete segments of currnt process
-                                process_id_d = segment_list[i].get_Process_ID();
-                                for (int k = 0; k < segment_list.Count(); k++)
-                                {
-                                    if (process_id_d == segment_list[k].get_Process_ID())
-                                    {
-                                        //delete segment
-                                        //dealocate
-                                    }
-                                }
-
-
-                            }
-
-                        }
                     }
-                    break;
+                    if (flag == 0)
+                    {
+                        p_id = segment_list[i].get_Process_ID();
+                        return false;
+                    }
                 }
-           
+
+            }
+
+            return true;
 
         }
 
-        
+
     }
 }
