a93b4d239ec7bb114e85f82b93f242afbf23c942
Heba-Atef99 <hoba30.ha@gmail.com>
2021-05-30 10:48:01 +0200

    deallocate error window is done

---
+++ deallocate_error/Deallocate.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace memory_blocks
+{
+    partial class Deallocate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.deallocate_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a process to deallocate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 139);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 37);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // deallocate_btn
+            // 
+            this.deallocate_btn.Location = new System.Drawing.Point(76, 224);
+            this.deallocate_btn.Name = "deallocate_btn";
+            this.deallocate_btn.Size = new System.Drawing.Size(238, 54);
+            this.deallocate_btn.TabIndex = 2;
+            this.deallocate_btn.Text = "Deallocate";
+            this.deallocate_btn.UseVisualStyleBackColor = true;
+            this.deallocate_btn.Click += new System.EventHandler(this.deallocate_btn_Click);
+            // 
+            // Deallocate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.deallocate_btn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Deallocate";
+            this.Text = "Deallocate";
+            this.Load += new System.EventHandler(this.Deallocate_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Deallocate_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button deallocate_btn;
+    }
+}
---
+++ deallocate_error/Deallocate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memory_blocks
+{
+    public partial class Deallocate : Form
+    {
+        public Deallocate()
+        {
+            InitializeComponent();
+        }
+
+        private void Deallocate_Load(object sender, EventArgs e)
+        {
+            string message = "Process 1 failed to allocate, do you want to deallocate another process from the memory?";
+            string title = "Allocation Error";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                Dictionary<Nullable<int>, string> comboSource = new Dictionary<Nullable<int>, string>();
+                //comboSource.Add(null, "");
+                comboSource.Add(1, "Sunday");
+                comboSource.Add(2, "Monday");
+
+                comboBox1.DataSource = new BindingSource(comboSource, null);
+                comboBox1.DisplayMember = "Value";
+                comboBox1.ValueMember = "Key";
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deallocate_btn_Click(object sender, EventArgs e)
+        {
+            Nullable<int> key = ((KeyValuePair<Nullable<int>, string>)comboBox1.SelectedItem).Key;
+            string value = ((KeyValuePair<Nullable<int>, string>)comboBox1.SelectedItem).Value;
+            MessageBox.Show(key + "   " + value);
+        }
+
+        private void Deallocate_Paint(object sender, PaintEventArgs e)
+        {
+            //dimensions of the rectangel
+            int width = 200;
+            int[] height = { 20, 40, 60, 50, 30 };
+
+            //margins of the rectangle inside the form  
+            int blocks_number = 5;
+            int x_margin = 400;
+            int[] y_margin = new int[blocks_number];
+
+            // Text specifications: pen, font
+            Pen black_pen = new Pen(Color.Black, 2);
+            Font text_font = new Font("Arial", 10, FontStyle.Regular, GraphicsUnit.Point);
+            string text;
+
+            // Create a StringFormat object with the each line of text, and the block
+            // of text centered on the page.
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+
+            for (int i = 0; i < blocks_number; i++)
+            {
+                text = "Segment " + i;
+
+                if (i == 0)
+                    y_margin[i] = 50;
+                else
+                    y_margin[i] = y_margin[i - 1] + height[i - 1];
+
+
+                //draw the addresses beside the rectangle    
+                e.Graphics.DrawString("1000", text_font, Brushes.Black, x_margin - 35, y_margin[i] - 8);
+
+                // Create rectangle.
+                Rectangle rect = new Rectangle(x_margin, y_margin[i], width, height[i]);
+                e.Graphics.DrawString(text, text_font, Brushes.Black, rect, stringFormat);
+
+                // Draw rectangle to screen.
+                e.Graphics.DrawRectangle(black_pen, rect);
+            }
+        }
+    }
+}
